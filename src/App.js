f31b7e74a13ad6a7c81454bb15824af0003264a8
vamseeuxd <vamsee.uxd@gmail.com>
2022-09-25 21:15:11 +0530

    fix: removed unwanted code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-/* import logo from './logo.svg';
-import { Counter } from './features/counter/Counter'; */
 import "./App.css";
 import { SideNav } from "./layout/side-nav/SideNav";
 
